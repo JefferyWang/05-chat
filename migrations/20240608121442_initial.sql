9520ddb2a4c60a5e421a5a3c363721c8617aa1f9
JefferyWang <admin@wangjunfeng.com>
2024-06-24 22:22:13 +0800

    feature: add triggers for chat/message

--- migrations/20240608121442_initial.sql
+++ migrations/20240608121442_initial.sql
@@ -3,12 +3,37 @@
 -- create user table
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
+    ws_id BIGINT NOT NULL,
     fullname VARCHAR(64) NOT NULL,
     email VARCHAR(64) NOT NULL,
     -- hashed argon2 password
     password_hash VARCHAR(97) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
+-- workspace for users
+CREATE TABLE IF NOT EXISTS workspaces (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE,
+    owner_id BIGINT NOT NULL REFERENCES users(id),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+-- add super user and workspace
+BEGIN;
+INSERT INTO users (id, ws_id, fullname, email, password_hash)
+VALUES (0, 0, 'super user', 'super@none.org', '');
+INSERT INTO workspaces (id, name, owner_id)
+VALUES (0, 'none', 0);
+COMMIT;
+
+-- add foreign key constraint for ws_id for users
+ALTER TABLE users
+    ADD CONSTRAINT users_ws_id_fk
+    FOREIGN KEY (ws_id)
+    REFERENCES workspaces(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
 
 -- create index for users for email
 CREATE UNIQUE INDEX IF NOT EXISTS email_index ON users(email);
@@ -24,6 +49,7 @@ CREATE TYPE chat_type AS ENUM (
 -- create chat table
 CREATE TABLE IF NOT EXISTS chats (
     id BIGSERIAL PRIMARY KEY,
+    ws_id BIGINT REFERENCES workspaces(id),
     name VARCHAR(64),
     type chat_type NOT NULL,
     -- user id list
